ed38bcc3cd16af7b17a3a715b3bea17fd6706cb9
krishna-dalam <krishnachaithanyadalam@gmail.com>
2020-07-24 16:00:45 +0530

    Swayamvar problem from TCS Code Vita question added

--- src/main/java/com/practice/leetcode/queue/Swayamvar.java
+++ src/main/java/com/practice/leetcode/queue/Swayamvar.java
@@ -3,6 +3,33 @@ package com.practice.leetcode.queue;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+    Swayamvar
+
+    Bride and Bridegrooms stand in a queue. Bridge has a lucky chance to choose her Bridegroom but conditions apply.
+    1. The alcohol type of Bride should match with Bridegroom.
+       There are 2 alcohol types namely r and m
+    2. If a Bride cannot find a groom every other bride waiting next to her in the queue will not get married.
+
+    Example 1:
+     Input:
+      brides = "rrmm"
+      bridegrooms = "mrmr"
+     Output:
+      0
+
+    Example 2:
+     Input:
+      brides = "rmrm"
+      bridegrooms = "mmmr"
+     Output:
+      2
+     Explanation:
+      Bride at first place only marries groom who drinks rum. So, groom joins back of queue and this will repeat until
+      match is not found. Since there is no 2nd r in the grooms list r and m will be unmarried
+
+ */
+
 public class Swayamvar {
     public static void main(String[] args) {
         int n= 4;
